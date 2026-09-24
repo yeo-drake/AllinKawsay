f386a1e592c00d8aa8030183bf50069345e55d68
Ye Kiu <76828853+yeo-drake@users.noreply.github.com>
2026-09-24 16:29:32 -0500

    Update datos_ejemplo.dart

--- lib/data/datos_ejemplo.dart
+++ lib/data/datos_ejemplo.dart
@@ -1,1 +1,37 @@
+import '../models/cancion.dart';
 
+final List<Cancion> cancionesEjemplo = [
+  Cancion(
+    id: '1',
+    titulo: 'El Cóndor Pasa',
+    compositor: 'Daniel Alomía Robles',
+    ritmo: 'Huayno',
+    region: 'Perú',
+    numerofonia: '5 5 6 5 | 3 3 5 3 | 2 2 3 2 | 1 1 2 1',
+    audioUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    pdfUrl: '',
+    descripcion: 'Clásico andino. Ejemplo de numerofonía básica.',
+  ),
+  Cancion(
+    id: '2',
+    titulo: 'Vírgenes del Sol',
+    compositor: 'Jorge Bravo de Rueda',
+    ritmo: 'Fox Incaico',
+    region: 'Perú',
+    numerofonia: '3 5 6 | 5 3 2 | 1 2 3 | 2 - -',
+    audioUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+    pdfUrl: '',
+    descripcion: 'Melodía tradicional andina.',
+  ),
+  Cancion(
+    id: '3',
+    titulo: 'Puno Canta',
+    compositor: 'Anónimo',
+    ritmo: 'Sikuri',
+    region: 'Puno',
+    numerofonia: '1 2 3 5 | 3 2 1 | 5 5 6 | 5 - -',
+    audioUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3',
+    pdfUrl: '',
+    descripcion: 'Sikuri tradicional puneño.',
+  ),
+];

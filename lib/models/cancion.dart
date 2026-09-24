2cc48cacbb62f2baf80c51a1a7447f6c44cdacd5
Ye Kiu <76828853+yeo-drake@users.noreply.github.com>
2026-09-24 16:28:48 -0500

    Update cancion.dart

--- lib/models/cancion.dart
+++ lib/models/cancion.dart
@@ -1,1 +1,23 @@
+class Cancion {
+  final String id;
+  final String titulo;
+  final String compositor;
+  final String ritmo;
+  final String region;
+  final String numerofonia;
+  final String audioUrl;
+  final String pdfUrl;
+  final String descripcion;
 
+  Cancion({
+    required this.id,
+    required this.titulo,
+    required this.compositor,
+    required this.ritmo,
+    required this.region,
+    required this.numerofonia,
+    required this.audioUrl,
+    required this.pdfUrl,
+    required this.descripcion,
+  });
+}

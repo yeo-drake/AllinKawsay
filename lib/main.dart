60b7ad3c1df9d1e2770a3fff7e23da28f221d9f9
Ye Kiu <76828853+yeo-drake@users.noreply.github.com>
2026-09-24 16:28:14 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
+void main() => runApp(const SikurisApp());
+
+class SikurisApp extends StatelessWidget {
+  const SikurisApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sikuris',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF8B4513),
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
